1e783c3cb61878fffaee2f7ccb13ffa4ce523e8a
ramblehead <v.rybynok@gmail.com>
2019-06-03 23:56:51 +0100

    Checkpoint

--- models/land-api.js
+++ models/land-api.js
@@ -30,3 +30,22 @@ prom.then(data => console.log('data returned:', data));
 
 
 prom = fetch('https://api.tvmaze.com/search/shows?q=batman');
+
+
+await fetch('http://localhost:8529/_db/_system/land', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
+  body: JSON.stringify({
+    query: `query GetElement {
+      element(id: "0000") {
+        collection
+        id
+        name
+        description
+      }
+    }`,
+  }),
+});
