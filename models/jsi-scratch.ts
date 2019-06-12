6e0a4cc5e2f3068d5ceaf8eace72cd52db0047bd
ramblehead <v.rybynok@gmail.com>
2019-06-12 20:07:50 +0100

    Checkpoint

---
+++ models/jsi-scratch.ts
@@ -0,0 +1,56 @@
+// Hey Emacs, this is -*- coding: utf-8 -*-
+
+import fetch from 'isomorphic-unfetch';
+
+prom = fetch('http://localhost:8529/_db/_system/land', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
+  body: JSON.stringify({
+    query: `{
+      query GetElement {
+        element(id: "0000") {
+          collection
+          id
+          name
+          description
+        }
+      }
+    }`,
+  }),
+})
+  .then(r => r.json())
+  .then(data => console.log('data returned:', data));
+
+await fetch('http://localhost:8529/_db/_system/land', {
+  method: 'POST',
+  headers: {
+      'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
+  body: JSON.stringify({
+    'query': `
+      query GetElement($id: String!) {
+        element(id: $id) {
+          collection
+          id
+          name
+          description
+        }
+      }
+    `,
+    variables: {
+      id: "0000",
+    },
+  }),
+}).then(r => r.json());
+
+function add(a: number, b: number) {
+  return a + b
+}
+
+add(1, 2);
+
+add('a', 'b');
--- models/land-api.ts
+++ models/land-api.ts
@@ -2,36 +2,14 @@
 
 import fetch from 'isomorphic-unfetch';
 
-prom = fetch('http://localhost:8529/_db/_system/land', {
+await fetch('http://localhost:8529/_db/_system/land', {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
   },
   body: JSON.stringify({
-    query: `{
-      query GetElement {
-        element(id: "0000") {
-          collection
-          id
-          name
-          description
-        }
-      }
-    }`,
-  }),
-})
-  .then(r => r.json())
-  .then(data => console.log('data returned:', data));
-
-await fetch('http://localhost:8529/_db/_system/land', {
-  method: 'POST',
-  headers: {
-      'Content-Type': 'application/json',
-    Accept: 'application/json',
-  },
-  body: JSON.stringify({
-    'query': `
+    query: `
       query GetElement($id: String!) {
         element(id: $id) {
           collection
@@ -42,15 +20,7 @@ await fetch('http://localhost:8529/_db/_system/land', {
       }
     `,
     variables: {
-      id: "0000",
+      id: '0000',
     },
   }),
-}).then(r => r.json());
-
-function add(a: number, b: number) {
-  return a + b
-}
-
-add(1, 2);
-
-add('a', 'b');
+}).then((r): any => r.json());
